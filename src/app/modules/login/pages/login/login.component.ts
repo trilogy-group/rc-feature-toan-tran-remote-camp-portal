3d2490b914f969222626e30760ffc44db965f744
daniel falla <daniel.falla@devfactory.com>
2019-06-21 08:58:15 -0500

    feat: added google sign in button

--- src/app/modules/login/pages/login/login.component.ts
+++ src/app/modules/login/pages/login/login.component.ts
@@ -1,14 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthenticationService } from 'src/app/shared/services/authentication.service';
+
+declare var gapi: any;
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
-
+export class LoginComponent implements OnInit {
   public form: FormGroup;
 
   public constructor(
@@ -20,6 +21,26 @@ export class LoginComponent {
       username: _formBuilder.control('', [Validators.required]),
       password: _formBuilder.control('', [Validators.required])
     });
+  }
+
+  public ngOnInit(): void {
+    gapi.signin2.render('my-signin2', {
+      'scope': 'profile email',
+      'width': 240,
+      'height': 40,
+      'longtitle': true,
+      'theme': 'dark',
+      'onsuccess': this.onSuccess,
+      'onfailure': this.onFailure
+    });
+  }
+
+  public onSuccess(googleUser: any): void {
+    console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
+  }
+
+  public onFailure(error: any): void {
+    console.log(error);
   }
 
   public login(): void {
