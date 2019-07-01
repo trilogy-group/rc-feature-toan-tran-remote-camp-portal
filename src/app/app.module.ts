e3c4afe073b08b6512afe0090af9042f2477c14c
daniel falla <daniel.falla@devfactory.com>
2019-07-01 16:18:31 -0500

    fix: fixed tooltips in hardest problems

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,17 @@ import { MatButtonModule, MatIconModule, MatListModule, MatMenuModule, MatToolti
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { DfSidebarModule, DfHttpLoaderInterceptorModule } from '@devfactory/ngx-df';
 
 import { AppRoutingModule } from 'src/app/app-routing.module';
 import { MainLayoutComponent } from 'src/app/layout/main/main-layout.component';
 import { AuthenticationGuard } from 'src/app/shared/guards/authentication.guard';
 import { AuthenticationService } from 'src/app/shared/services/authentication.service';
 import { AccomplishmentsService } from 'src/app/shared/services/accomplishments.service';
+import { AppComponent } from 'src/app/app.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
-import { DfSidebarModule, DfHttpLoaderInterceptorModule } from '@devfactory/ngx-df';
-import { AppComponent } from './app.component';
-import { SharedModule } from './shared/shared.module';
-import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -32,7 +32,7 @@ import { HttpClientModule } from '@angular/common/http';
     SharedModule,
     HttpClientModule,
     HttpClientModule,
-    DfHttpLoaderInterceptorModule.forRoot()
+    DfHttpLoaderInterceptorModule.forRoot(),
   ],
   providers: [
     AuthenticationGuard,
