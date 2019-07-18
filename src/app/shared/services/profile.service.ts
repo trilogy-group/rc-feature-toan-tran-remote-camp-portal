405de68815165308f971567724d5445e61ee9894
daniel falla <daniel.falla@devfactory.com>
2019-07-17 20:29:24 -0500

    feat: getting profile settings

--- src/app/shared/services/profile.service.ts
+++ src/app/shared/services/profile.service.ts
@@ -1,17 +1,17 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+import { environment } from 'src/environments/environment';
 
 @Injectable()
 export class ProfileService {
-  private readonly profile = {
-    xoId: 'John Smith',
-    jiraId: 'John Smith',
-    companyEmail: 'john.smith@aurea.com',
-    icName: 'John Smith',
-  };
+  private GET_PROFILE_SETTINGS = `${environment.apiUrl}/Profile`;
+
+  public constructor(private httpClient: HttpClient) { }
 
   public getProfile(): Observable<any> {
-    return of(this.profile);
+    return this.httpClient.get(this.GET_PROFILE_SETTINGS);
   }
 
   public saveProfile(profile: any): Observable<any> {
