e709153516a73ec8d8010ee1f7b79717505625dd
daniel falla <daniel.falla@devfactory.com>
2019-07-30 19:12:29 -0500

    fix: added route guards for each role

--- src/app/shared/guards/authentication.guard.ts
+++ src/app/shared/guards/authentication.guard.ts
@@ -14,15 +14,49 @@ export class AuthenticationGuard implements CanActivate {
   ) {}
 
   public canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    if (route.url.toString().startsWith(`${environment.apiUrl}/gradebook`)) {
-      return this._authenticationTokenService.isUserAdmin();
-    }
-    if (this._authenticationTokenService.isLoggedIn()) {
-      return true;
-    } else {
-      this._router.navigate(['login']);
+    const isUserAdmin = this._authenticationTokenService.isUserAdmin();
+    const isLoggedIn = this._authenticationTokenService.isLoggedIn();
+
+    if (!isLoggedIn) {
+      this._router.navigate(['/login']);
+      return false;
     }
 
+    if (isLoggedIn && !route.firstChild) {
+      isUserAdmin ? this._router.navigate(['/gradebook']) : this._router.navigate(['/dashboard']);
+    }
+
+    if (isLoggedIn && route.firstChild) {
+      const url = route.firstChild.url ? route.firstChild.url[0].path : '';
+      if (url === '') {
+        this._router.navigate(['/']);
+        return false;
+      }
+
+      if (isUserAdmin && url !== '') {
+        if (this.isAdminRoute(url)) {
+          return true;
+        } else {
+          this._router.navigate(['/']);
+          return false;
+        }
+      }
+
+      if (this.isUserRoute(url)) {
+        return true;
+      } else {
+        this._router.navigate(['/']);
+        return false;
+      }
+    }
     return false;
+  }
+
+  private isAdminRoute(url: string): boolean {
+    return url === 'gradebook';
+  }
+
+  private isUserRoute(url: string): boolean {
+    return url !== 'gradebook';
   }
 }
