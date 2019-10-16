c15267240775bb97d3db8b843e64bf6f99cb9257
Alex Netrebskiy <alex.netrebskiy@aurea.com>
2019-10-16 19:17:01 +0400

    Update links for google-check-drive

--- src/environments/environment.qa.ts
+++ src/environments/environment.qa.ts
@@ -10,7 +10,7 @@ export const environment = {
   remoteUMaterialsReportIssueGetUrl: 'https://s1w5hujwr4.execute-api.us-east-1.amazonaws.com/qa/report-issue-remote-u-materials-get',
   codeRepositoryIssueGetUrl: 'https://s1w5hujwr4.execute-api.us-east-1.amazonaws.com/qa/reporting_issue_code_repo_get',
   reportIssuePostUrl: 'https://s1w5hujwr4.execute-api.us-east-1.amazonaws.com/qa/reporting_issue_post',
-  checkAssignmentFolderGetUrl: 'https://aajp1n4sv1.execute-api.us-east-1.amazonaws.com/development/check-drive',
+  checkAssignmentFolderGetUrl: 'https://3nkouc8pv2.execute-api.us-east-1.amazonaws.com/qa/check-drive',
   readyToStartGetUrl: 'https://s1w5hujwr4.execute-api.us-east-1.amazonaws.com/qa/ready-to-start-get',
   jiraStatusGetUrl: 'https://yjc5fqkq9c.execute-api.us-east-1.amazonaws.com/development/jira-check',
   ticketsAssignedGetUrl: 'https://sr202yf402.execute-api.us-east-1.amazonaws.com/qa/backlog_status',
